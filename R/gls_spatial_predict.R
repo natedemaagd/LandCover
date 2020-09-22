4882019c5e0ce41ca3006a6407dae06a0dc15cdd
Nathan DeMaagd <nate.demaagd@gmail.com>
2020-09-22 12:06:01 -1000

    work on gls_spatial_predict

--- R/gls_spatial_predict.R
+++ R/gls_spatial_predict.R
@@ -7,11 +7,11 @@
 #'
 #' @param data `data.frame` with spatial data
 #' @param reg_results object of class `gls` from the `gls_spatial` function
-#' @param lc value of landcover for which the new dependent variable values are to be predicted
+#' @param lc vector of landcovers for which the new dependent variable values are to be predicted
 #'
-#' @return A list whose elements are the optimal GLS model. See details.
+#' @return A vector with predicted values of the dependent variable from gls_spatial()
 #'
-#' @details Landcovers with large numbers of observations will take a long time to run. It is strongly recommended to use 5 cores in parallel. The code runs the landcover types sequentially, and the spatial GLS models for each landcover in parallel. The function will output the landcover and time of completion as it progresses, unless `silent = TRUE`. If there are too many observations for your system, you must take a representative subset. Each list element in the output corresponds to a landcover type. The objects are of class `gls` and will report the correlation structure that resulted in the lowest model AIC. If an element in the output list is `NA`, the log likelihood model failed to converge using all correlation structures. Try increasing the number of iterations or increasing the tolerance.
+#' @details
 #'
 #' @examples
 #' # create data.frame with x,y coordinates, landcover variable, and two covariates
@@ -23,13 +23,16 @@
 #' # run the gls model
 #' gls_results <- gls_spatial(data = dat, landcover_varname = 'lc', landcover_vec = c('a', 'b'), reg_formula = var3 ~ var1 + var2, error_formula = ~x+y)
 #'
+#' # add predicted values of var3 to the data.frame
+#' dat$var3_predict <- gls_spatial_predict(data = dat, reg_results = gls_results, landcover = unique(dat$lc))
+#'
 #' @export
 
 
 ### FUNCTION:
-gls_spatial_predict <- function(data, reg_results, lc){
+gls_spatial_predict <- function(data, reg_results, landcover){
 
-  predict(object = reg_results[names(reg_results) == as.character(lc)][[1]], newdata = data)
+  predict(object = reg_results[names(reg_results) %in% as.character(landcover)][[1]], newdata = data)
 
 }
 
